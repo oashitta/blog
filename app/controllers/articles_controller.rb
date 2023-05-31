a9906528cb13c0c030da778959c2297396e08e0c
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-30 23:18:08 -0400

    adding show view for individual posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,10 @@
 class ArticlesController < ApplicationController
   # action/method
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
